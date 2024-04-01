e610b8f18b21e6b3b9d1950642d8dceb49d2d4a1
isalip48 <100031700+isalip48@users.noreply.github.com>
2024-04-01 23:52:36 +0530

    Update dashboard-student.tsx

--- Student_Management_System--main/client/app/components-deleted/studentPanel/dashboard-student.tsx
+++ Student_Management_System--main/client/app/components-deleted/studentPanel/dashboard-student.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Overview from './overview';
-import Courses from '../../student/courses/page';
 import Profile from './profile';
 
 export default function Dashboard() {
